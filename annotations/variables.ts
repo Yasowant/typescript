3eab9b871cd3301c65c9e512a3899485eb4878ee
Yasowant <yasowant1998@gmail.com>
2024-05-22 09:49:42 +0530

    update code variables some advace concept

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -27,3 +27,27 @@ console.log(point);
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+//1.Function that returns the 'any' type
+const json = '{"x":10,"y":34}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates);
+
+//2.when we declare a variable on one line and initalize it later
+let words = ['red', 'green', 'blue'];
+let findword = false;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    findword = true;
+  }
+}
+
+//3.Variable which type can not be inferred
+let numbers = [-10, -2, 9];
+let numberAboveZero: boolean | number = false;
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
